8f70b3c950d20dfd64b2d994194a9e71bf1575e0
toms3776c <24806125+toms3776c@users.noreply.github.com>
2023-12-12 22:48:30 +0900

    ユーザー登録機能追加

---
+++ app/api/user/register/route.js
@@ -0,0 +1,15 @@
+import connectDB from "@/app/utils/database";
+import { UserModel } from "@/app/utils/schemaModels";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+    const reqBody = await request.json()
+    
+    try {
+        await connectDB()
+        await UserModel.create(reqBody)
+        return NextResponse.json({message: "ユーザー登録 成功"})
+    } catch (error) {
+        return NextResponse.json({message: "ユーザー登録 失敗"})
+    }
+}
