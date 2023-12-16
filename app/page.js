5e45171fbecd65a91c33e5e481d952f04e8a1a57
toms3776c <24806125+toms3776c@users.noreply.github.com>
2023-12-16 13:25:34 +0900

    リンク設定追加、コード整形

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const getAllItems = async () => {
   const response = await fetch("http://localhost:3000/api/item/readall", {
     cache: "no-store",
@@ -12,12 +15,22 @@ const ReadAllItems = async () => {
   return (
     <div>
       <h1 className="h1-style">こんにちは</h1>
-      {allItems.map(item => <div key={item._id}>
-        <img src={item.image}/>
-        <h2>{item.price}</h2>
-        <h3>{item.title}</h3>
-        <p>{item.description}</p>
-      </div>)}
+      {allItems.map((item) => (
+        <Link href="" key={item._id}>
+          <Image
+            src={item.image}
+            width={750}
+            height={500}
+            alt="item-image"
+            priority
+          />
+          <div key={item._id}>
+            <h2>{item.price}</h2>
+            <h3>{item.title}</h3>
+            <p>{item.description.substring(0, 80)}...</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
