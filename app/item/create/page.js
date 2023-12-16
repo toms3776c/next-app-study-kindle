dc8f226ecdd4f272d490a02d526c7a25e294affb
toms3776c <24806125+toms3776c@users.noreply.github.com>
2023-12-16 15:29:02 +0900

    アイテム作成ページを追加

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -1,9 +1,79 @@
-const Register = () => {
+"use client";
+
+import { useState } from "react";
+
+const CreateItem = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/api/item/create", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({
+          title: title,
+          price: price,
+          image: image,
+          description: description,
+          email: "dummy-data",
+        }),
+      });
+      const JsonData = await response.json();
+      alert(JsonData.message);
+    } catch (error) {
+      alert("アイテム作成 失敗");
+    }
+  };
+
   return (
     <div>
-      <h1>ユーザー登録</h1>
+      <h1>アイテム作成</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          name="title"
+          placeholder="アイテム名"
+          required
+        />
+        <input
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          type="text"
+          name="price"
+          placeholder="価格"
+          required
+        />
+        <input
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          type="text"
+          name="image"
+          placeholder="画像"
+          required
+        />
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          name="description"
+          rows={15}
+          placeholder="商品説明"
+          required
+        ></textarea>
+        <button>作成</button>
+      </form>
     </div>
   );
 };
 
-export default Register;
+export default CreateItem;
--- middleware.js
+++ middleware.js
@@ -2,8 +2,7 @@ import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-    // const token = await request.headers.get("Authorization")?.split(" ")[1]
-    const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGV4YW1wbGUuY29tIiwiZXhwIjoxNzAyNTU2MTEyfQ.ZkzmcZ3aWumfDwDXQwkwwHf4d4GYI89HVimvh72P1UU"  // デバッグ用
+    const token = await request.headers.get("Authorization")?.split(" ")[1]
     if(!token){
         return NextResponse.json({message: "トークンがありません"})
     }
