0795a0409e0e8f980bf73dce81092265fb85cbf0
toms3776c <24806125+toms3776c@users.noreply.github.com>
2023-12-16 13:20:44 +0900

    MongoDBから取得したデータをもとにトップページを作成するコードを追加

--- app/page.js
+++ app/page.js
@@ -1,10 +1,25 @@
-const ReadAllItems = () => {
+const getAllItems = async () => {
+  const response = await fetch("http://localhost:3000/api/item/readall", {
+    cache: "no-store",
+  });
+  const jsonData = await response.json();
+  const allItems = jsonData.allItems;
+  return allItems;
+};
+
+const ReadAllItems = async () => {
+  const allItems = await getAllItems();
   return (
     <div>
       <h1 className="h1-style">こんにちは</h1>
-      <h3>さようなら</h3>
+      {allItems.map(item => <div key={item._id}>
+        <img src={item.image}/>
+        <h2>{item.price}</h2>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+      </div>)}
     </div>
-  )
-}
+  );
+};
 
-export default ReadAllItems
+export default ReadAllItems;
